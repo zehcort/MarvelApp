4eb19b6b12eeafa6569005b935c92b44b8a43ed1
Luis Alejandro Tróchez Arredondo <luis.trochez@LUISTROCHEZ.pragma.com.co>
2020-08-21 17:38:06 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Dialogs } from '@ionic-native/dialogs/ngx';
 
 @Component({
   selector: 'app-root',
@@ -13,14 +14,15 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private dialogs: Dialogs
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
+      this.statusBar.styleLightContent();
       this.splashScreen.hide();
     });
   }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  comics: any;
 
-  constructor() {}
+  constructor(public http: HttpClient, public splashScreen: SplashScreen) {
+    this.getComics();
+  }
+
+  getComics(){
+    this.splashScreen.show();
+    const url = 'http://gateway.marvel.com/v1/public/comics?ts=1&apikey=b5dd158dd0e856443db7fb726fbc6bc9&hash=80182fcb24c6426319114b9e34eafed6';
+    this.http.get(url).subscribe(res => {
+      if(res['code'] == 200){
+        this.comics = res['data'].results;
+        for(let comicTemp of this.comics){
+          comicTemp.likes = 0;
+        }
+      } else{
+        this.getComics();
+      }
+
+    });
+    this.splashScreen.hide();
+  }
+
+  increaseLikes(comic){
+    comic.likes = comic.likes + 1;
+  }
+
+  decreaseLikes(comic){
+    comic.likes = comic.likes - 1;
+  }
 
 }
